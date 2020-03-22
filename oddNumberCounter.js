44a356b528561f129f1e31c26ddd5db071e1fc7b
arthurmatic <arthurshur@gmail.com>
2020-03-23 00:11:02 +0100

    update oddNumberCounter.js

--- oddNumberCounter.js
+++ oddNumberCounter.js
@@ -1,5 +1,8 @@
 let myName = 'Iniubong Arthur';
 let myCourses = 'HTML, CSS, JavaScript';
+
+console.log('My name is ' + myName);
+console.log('My courses are ' + myCourses);
 for (let i = 1; i < 200; i++) { 
     if ( i % 2 === 1) {
     let oddNumbers = i;
